6667c97465f4ec0f9e8a68af683f94d309cd811c
Yannis Markonis <imarkonis@gmail.com>
2017-11-27 16:03:23 +0100

    cleaning syntax and var names

--- database/import_data.R
+++ database/import_data.R
@@ -3,7 +3,7 @@ library(data.table)
 
 setwd("~/sync/Various_Proj/drought_uncertainty/database") ## Local path where
 
-force2geo = function(force, mhm){
+force2geo <- function(force, mhm){
   
   force = flip(force, direction = 'y')
   xmin(force) <- xmin(mhm)
@@ -11,73 +11,72 @@ force2geo = function(force, mhm){
   ymax(force) <- ymax(mhm)
   ymin(force) <- ymin(mhm)
   
-  force = mask(force, mhm)
+  force <- mask(force, mhm)
 }
 
-rb = readRDS('./database/mha/geo/ccm-regs.rds')    
-reg = readRDS('./database/mha/geo/srex-regs.rds')  
-mhm = raster('./database/mha/geo/mHM_grid.tif') 
+rb <- readRDS('./database/mha/geo/ccm-regs.rds')    
+reg <- readRDS('./database/mha/geo/srex-regs.rds')  
+mhm <- raster('./database/mha/geo/mHM_grid.tif') 
 
 ##################################################### Precipitation
 ##### Filenames and locations
-filename = "casty_cru_pre_mhm.nc"
-data_path = "./data/"
-resolution = 48000
+filename <- "casty_cru_pre_mhm.nc"
+data_path <- "./data/"
+resolution <- 48000
 
 ##### Import as raster
-casty_P_ras = brick(x = paste0(data_path, filename))
-casty_P_ras = force2geo(casty_P_ras,mhm)
+casty_pre_ras <- brick(x = paste0(data_path, filename))
+casty_pre_ras <- force2geo(casty_pre_ras,mhm)
 
-europe_lat = seq(casty_P_ras@extent@ymin, casty_P_ras@extent@ymax, resolution)
-europe_lon = seq(casty_P_ras@extent@xmin, casty_P_ras@extent@xmax, resolution)
-europe_grid = expand.grid(europe_lon, europe_lat)
+europe_lat_y <- seq(casty_pre_ras@extent@ymin, casty_pre_ras@extent@ymax, resolution)
+europe_lon_y <- seq(casty_pre_ras@extent@xmin, casty_pre_ras@extent@xmax, resolution)
+europe_grid <- expand.grid(europe_lon_y, europe_lat_y)
 
-casty_P_dt = t(extract(casty_P_ras, SpatialPoints(europe_grid)))
-dim(casty_P_dt)
+casty_pre_dt <- t(extract(casty_pre_ras, SpatialPoints(europe_grid)))
+dim(casty_pre_dt)
 
-nnames = apply(europe_grid, 1, paste, collapse = " ")
-colnames(casty_P_dt) = nnames
+nnames <- apply(europe_grid, 1, paste, collapse = " ")
+colnames(casty_pre_dt) = nnames
 
 ##### Transform to data.table format
-casty_P_dt = data.table(melt(casty_P_dt))
-casty_P_dt = casty_P_dt[complete.cases(casty_P_dt)]
-casty_P_dt[, year := as.numeric(substr(Var1, 2, 5))]
-casty_P_dt[, month := as.numeric(substr(Var1, 7, 8))]
-casty_P_dt[, Var2 := as.character(Var2)]
-casty_P_dt[, lon := as.numeric(sapply(strsplit(Var2,split=" ") , "[[", 1))]
-casty_P_dt[, lat := as.numeric(sapply(strsplit(Var2,split=" ") , "[[", 2))]
-casty_P_dt[, Var1 := NULL]
-casty_P_dt[, Var2 := NULL]
-setcolorder(casty_P_dt, c("year", "month", "lon", "lat", "value")) 
-colnames(casty_P_dt)[5] = "precip"
-
-#plot(casty_P_dt)
+casty_pre_dt = data.table(melt(casty_pre_dt))
+casty_pre_dt = casty_pre_dt[complete.cases(casty_pre_dt)]
+casty_pre_dt[, year := as.numeric(substr(Var1, 2, 5))]
+casty_pre_dt[, month := as.numeric(substr(Var1, 7, 8))]
+casty_pre_dt[, Var2 := as.character(Var2)]
+casty_pre_dt[, lon_y := as.numeric(sapply(strsplit(Var2, split = " ") , "[[", 1))]
+casty_pre_dt[, lat_y := as.numeric(sapply(strsplit(Var2, split = " ") , "[[", 2))]
+casty_pre_dt[, Var1 := NULL]
+casty_pre_dt[, Var2 := NULL]
+setcolorder(casty_pre_dt, c("year", "month", "lon_y", "lat_y", "value")) 
+colnames(casty_pre_dt)[5] = "pre"
 
 ##################################################### PET
 ##### Filenames and locations
 filename = "casty_cru_pet_mhm.nc"
 
 ##### Import as raster
-casty_PET_ras = brick(x = paste0(data_path, filename))
-casty_PET_ras = force2geo(casty_PET_ras,mhm)
+casty_pet_ras <- brick(x = paste0(data_path, filename))
+casty_pet_ras <- force2geo(casty_pet_ras,mhm)
 
-casty_PET_dt = t(extract(casty_PET_ras, SpatialPoints(europe_grid)))
+casty_pet_dt <- t(extract(casty_pet_ras, SpatialPoints(europe_grid)))
 
-nnames = apply(europe_grid, 1, paste, collapse = " ")
-colnames(casty_PET_dt) = nnames
+nnames <- apply(europe_grid, 1, paste, collapse = " ")
+colnames(casty_pet_dt) = nnames
 
 ##### Transform to data.table format
-casty_PET_dt = data.table(melt(casty_PET_dt))
-casty_PET_dt = casty_PET_dt[complete.cases(casty_PET_dt)]
-casty_PET_dt[, year := as.numeric(substr(Var1, 2, 5))]
-casty_PET_dt[, month := as.numeric(substr(Var1, 7, 8))]
-casty_PET_dt[, Var2 := as.character(Var2)]
-casty_PET_dt[, lon := as.numeric(sapply(strsplit(Var2,split=" ") , "[[", 1))]
-casty_PET_dt[, lat := as.numeric(sapply(strsplit(Var2,split=" ") , "[[", 2))]
-casty_PET_dt[, Var1 := NULL]
-casty_PET_dt[, Var2 := NULL]
-setcolorder(casty_PET_dt, c("year", "month", "lon", "lat", "value")) 
-colnames(casty_PET_dt)[5] = "PET"
+casty_pet_dt <- data.table(melt(casty_pet_dt))
+casty_pet_dt <- casty_pet_dt[complete.cases(casty_pet_dt)]
+casty_pet_dt[, year := as.numeric(substr(Var1, 2, 5))]
+casty_pet_dt[, month := as.numeric(substr(Var1, 7, 8))]
+casty_pet_dt[, Var2 := as.character(Var2)]
+casty_pet_dt[, lon_y := as.numeric(sapply(strsplit(Var2,split=" ") , "[[", 1))]
+casty_pet_dt[, lat_y := as.numeric(sapply(strsplit(Var2,split=" ") , "[[", 2))]
+casty_pet_dt[, Var1 := NULL]
+casty_pet_dt[, Var2 := NULL]
+setcolorder(casty_pet_dt, c("year", "month", "lon_y", "lat_y", "value")) 
+colnames(casty_pet_dt)[5] = "pet"
+
 
 
 
